b3e3ef452eb09d0cbc5cbcf0482965632c045731
Yarith Joseph <yarithjoseph@gmail.com>
2016-10-05 18:01:16 +0200

    screw you

---
+++ Includes/main.js
@@ -0,0 +1,22 @@
+$(document).ready()
+
+$("#welcome" ).hide();
+$("#welcome").fadeIn(4000)
+
+
+
+$("#welcome2" ).hide();
+$("#welcome2").fadeIn(7000);
+
+$("#welcome3" ).hide();
+$("#welcome3").fadeIn(10000)
+
+
+
+
+
+
+
+
+
+
